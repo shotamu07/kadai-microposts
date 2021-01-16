d558c93e6f0a209b808cac26f109f38beb7daf4c
shota.sakanoue <s-mrchil07.7@ezweb.ne.jp>
2021-01-16 03:11:49 +0000

    add favorites

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+  
   resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followings
@@ -13,6 +14,13 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :likes
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
