347e4e91569fa83ce436cb3b48d727504e66389c
shota.sakanoue <s-mrchil07.7@ezweb.ne.jp>
2021-01-20 13:04:54 +0000

    課題の修正01/20 22:04

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_133810) do
+ActiveRecord::Schema.define(version: 2021_01_16_051327) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_01_06_133810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["micropost_id"], name: "index_favorites_on_micropost_id"
-    t.index ["user_id", "micropost_id"], name: "index_favorites_on_user_id_and_micropost_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
